f23cb3bdcc02a83e915eeae3c2fc3d49f859edd0
rtan5 <rachelpraisetan@gmail.com>
2026-03-29 00:17:42 -0400

    t
    t

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -3,8 +3,7 @@ using TMPro;
 
 public class GameOver : MonoBehaviour
 {
-    public GameObject GameOverUI;
-    public PointSystem pointSystem;
+    //public PointSystem pointSystem;
 
     public TMP_Text points;
     public TMP_Text lives;
@@ -12,13 +11,12 @@ public class GameOver : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameOverUI.SetActive(true);
-        points.text = "Points: " + pointSystem.points;
-        lives.text = "Lives Left: " + pointSystem.lives;
+        //points.text = "Points: " + pointSystem.points;
+        //lives.text = "Lives Left: " + pointSystem.lives;
     }
 
     public void restartGame() {
-        
+
     }
 
     // Update is called once per frame
